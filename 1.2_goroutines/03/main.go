4001bc0f3061ff134bcf95342ae9131e85daf970
Aleksandr Kozlov <avlkozlov@avito.ru>
2024-11-06 03:31:14 +0300

    goroutines done

---
+++ 1.2_goroutines/03/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+// nextFunc возвращает следующее слово из генератора
+type nextFunc func() string
+
+// counter хранит количество цифр в каждом слове.
+// ключ карты - слово, а значение - количество цифр в слове.
+type counter map[string]int
+
+// pair хранит слово и количество цифр в нем
+type pair struct {
+	word  string
+	count int
+}
+
+// countDigitsInWords считает количество цифр в словах,
+// выбирая очередные слова с помощью next()
+func countDigitsInWords(next nextFunc) counter {
+	counted := make(chan pair)
+
+	// начало решения
+
+	go func() {
+		for {
+			word := next()
+			if word == "" {
+				break
+			}
+			count := countDigits(word)
+			counted <- pair{word: word, count: count}
+		}
+		close(counted)
+	}()
+
+	// Считайте значения из канала counted
+	// и заполните stats.
+	stats := make(counter)
+	for pair := range counted {
+		stats[pair.word] = pair.count
+	}
+
+	// В результате stats должна содержать слова
+	// и количество цифр в каждом.
+
+	// конец решения
+
+	return stats
+}
+
+// countDigits возвращает количество цифр в строке
+func countDigits(str string) int {
+	count := 0
+	for _, char := range str {
+		if unicode.IsDigit(char) {
+			count++
+		}
+	}
+	return count
+}
+
+// printStats печатает слова и количество цифр в каждом
+func printStats(stats counter) {
+	for word, count := range stats {
+		fmt.Printf("%s: %d\n", word, count)
+	}
+}
+
+// wordGenerator возвращает генератор, который выдает слова из фразы
+func wordGenerator(phrase string) nextFunc {
+	words := strings.Fields(phrase)
+	idx := 0
+	return func() string {
+		if idx == len(words) {
+			return ""
+		}
+		word := words[idx]
+		idx++
+		return word
+	}
+}
+
+func main() {
+	phrase := "0ne 1wo thr33 4068"
+	next := wordGenerator(phrase)
+	stats := countDigitsInWords(next)
+	printStats(stats)
+}
---
+++ 1.2_goroutines/go.mod
@@ -0,0 +1,3 @@
+module github.com/kozl/thank-go-multitasking/1.2_goroutines
+
+go 1.23.1
